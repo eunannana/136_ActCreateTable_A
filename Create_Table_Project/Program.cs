2f8a9dcf9246d84ada1e0fae7d66cdf7ffd256a8
eunannana <thisiseunkim93@gmail.com>
2022-03-16 14:48:55 +0700

    make new method to make new table operation in database with method name CreateTable()

--- Create_Table_Project/Program.cs
+++ Create_Table_Project/Program.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Create_Table_Project
 {
-    internal class Program
+    class Program
     {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source =ANN\\ANNSANDRYN; database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar (50), Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Table is succesfully created!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, looks like something wrong... " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         static void Main(string[] args)
         {
         }
